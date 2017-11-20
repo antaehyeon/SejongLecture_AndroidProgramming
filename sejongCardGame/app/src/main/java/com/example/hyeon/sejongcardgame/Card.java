d5d03d894932e4676a2cf1374f4a9e84a44f7c3b
안태현 <antaehyeon@live.co.kr>
2017-11-21 02:15:39 +0900

    Add Card Class - class 생성 - 카드 상태 정의 및 멤버 변수 초기화 - 카드 앞면 이미지 정의

---
+++ sejongCardGame/app/src/main/java/com/example/hyeon/sejongcardgame/Card.java
@@ -0,0 +1,28 @@
+package com.example.hyeon.sejongcardgame;
+
+/**
+ * Created by hyeon on 2017. 11. 21..
+ */
+
+public class Card {
+    // 1. 카드 상태 상수 정의
+    public static final int CARD_SHOW = 0;
+    public static final int CARD_CLOSE = 1;
+    public static final int CARD_PLAYEROPEN = 2;
+    public static final int CARD_MATCHED = 0;
+
+    private static final int IMG_RED = 1;
+    private static final int IMG_GREEN = 2;
+    private static final int IMG_BLUE = 3;
+
+    // 2. 카드 상태 멤버 변수 선언 및 초기화
+    public int m_State,
+               m_Color;
+
+    public Card(int _Color) {
+        m_State = CARD_SHOW;
+        m_Color = _Color;
+    } // Constructor
+
+    
+} // Card Class
