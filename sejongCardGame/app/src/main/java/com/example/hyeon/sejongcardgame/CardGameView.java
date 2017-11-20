920ea4731350912fb726bf667f8add7bc1718022
안태현 <antaehyeon@live.co.kr>
2017-11-21 02:05:17 +0900

    CardGameView Update - 배경 이미지 그리기

--- sejongCardGame/app/src/main/java/com/example/hyeon/sejongcardgame/CardGameView.java
+++ sejongCardGame/app/src/main/java/com/example/hyeon/sejongcardgame/CardGameView.java
@@ -1,6 +1,8 @@
 package com.example.hyeon.sejongcardgame;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.view.View;
 
@@ -9,13 +11,19 @@ import android.view.View;
  */
 
 public class CardGameView extends View {
+    // 1. 멤버 변수 추가
+    Bitmap m_BackGroundImage;
 
     public CardGameView(Context context) {
         super(context);
+        // 2. 멤버 변수 설정
+        m_BackGroundImage = BitmapFactory.decodeResource(getResources(),
+                R.drawable.background, null);
     }
 
     @Override
     public void onDraw(Canvas canvas) {
-
+        // 3. 배경 이미지 그리기
+        canvas.drawBitmap(m_BackGroundImage, 0, 0, null);
     }
 }
