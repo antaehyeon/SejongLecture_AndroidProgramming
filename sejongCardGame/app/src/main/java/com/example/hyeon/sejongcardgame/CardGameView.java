aee51b31b3521222bc2343cede332016949ac349
안태현 <antaehyeon@live.co.kr>
2017-11-21 02:10:03 +0900

    CardGameView Update - 카드 뒷면 이미지 그리기

--- sejongCardGame/app/src/main/java/com/example/hyeon/sejongcardgame/CardGameView.java
+++ sejongCardGame/app/src/main/java/com/example/hyeon/sejongcardgame/CardGameView.java
@@ -12,18 +12,27 @@ import android.view.View;
 
 public class CardGameView extends View {
     // 1. 멤버 변수 추가
-    Bitmap m_BackGroundImage;
+    Bitmap m_BackGroundImage,
+           m_CardBackSide;
 
     public CardGameView(Context context) {
         super(context);
         // 2. 멤버 변수 설정
         m_BackGroundImage = BitmapFactory.decodeResource(getResources(),
                 R.drawable.background, null);
-    }
+        m_CardBackSide = BitmapFactory.decodeResource(getResources(),
+                R.drawable.backside, null);
+    } // constructor
 
     @Override
     public void onDraw(Canvas canvas) {
         // 3. 배경 이미지 그리기
         canvas.drawBitmap(m_BackGroundImage, 0, 0, null);
-    }
-}
+        for (int y = 0; y < 2; y++) {
+            for (int x = 0; x < 3; x++) {
+                canvas.drawBitmap(m_CardBackSide, 35 + x * 90,
+                        150 + y * 130, null);
+            } // for x
+        } // for y
+    } // onDraw
+} // CardGameView Class
