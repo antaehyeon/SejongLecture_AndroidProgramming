936a2244073e338bfec0d9389ac29e738e33cb82
안태현 <antaehyeon@live.co.kr>
2017-12-11 20:07:01 +0900

    Update EventHandlingActivity, activity_event_handling - 분할된 프로젝트로 병합 - GestureDetector 클래스 사용

---
+++ Android_Lecture_example/app/src/main/java/com/example/hyeon/sejonglectureandroidprogramming/EventHandlingActivity.java
@@ -0,0 +1,98 @@
+package com.example.hyeon.sejonglectureandroidprogramming;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class EventHandlingActivity extends AppCompatActivity {
+
+    View firstView;
+
+    TextView textView;
+
+    GestureDetector detector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_handling);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        firstView = (View) findViewById(R.id.view);
+        firstView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                int action = motionEvent.getAction();
+
+                float curX = motionEvent.getX();
+                float curY = motionEvent.getY();
+
+                if (action == MotionEvent.ACTION_DOWN) {
+                    println("손가락 눌림 : " + curX + ", " + curY);
+                } else if (action == MotionEvent.ACTION_MOVE) {
+                    println("손가락 움직임 : " + curX + ", " + curY);
+                } else if (action == MotionEvent.ACTION_UP) {
+                    println("손가락 뗌 : " + curX + ", " + curY);
+                }
+
+                return true;
+            }
+        });
+
+        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                println("onDown() 호출됨");
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+                println("onShowPress() 호출됨");
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                println("onSingleTapUp() 호출됨");
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println("onScroll() 호출됨 : " + v + ", " + v1);
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+                println("onLongPress() 호출됨");
+            }
+
+            @Override
+            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println("onFling() 호출됨 : " + v + ", " + v1);
+                return true;
+            }
+        });
+
+        View view2 = findViewById(R.id.view2);
+        view2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                detector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
+
+    } // onCreate
+
+    public void println(String data) {
+        textView.append(data + "\n");
+    }
+
+}
